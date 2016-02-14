a0731543d243ec861adbc72630374a8d407e9870
Hatim Alam <hatimalam@gmail.com>
2016-02-14 15:34:30 +0530

    Create en_us.sudo_user_login.php
    Added labels in new language file.

---
+++ Files/en_us.sudo_user_login.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Language strings for sugarcrm_sudo_login package
+ * Written by: Hatim Alam
+ * Dated: 14th Feb 2016
+ */
+
+$mod_strings['LBL_USER_LOGIN_AS'] = 'Login As';
+$mod_strings['LBL_USER_LOGIN_BACK_AS'] = 'Login Back As';
